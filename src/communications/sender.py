fbc317b4f5a50f1c3e7ba1c496838bb3a375a0ed
beyondTheMyth <herculesgenitsi@gmail.com>
2021-01-14 17:55:06 +0200

    Add sender communication script

---
+++ src/communications/sender.py
@@ -0,0 +1,28 @@
+__module_name__ = "ax_eva"
+__module_version__ = "0.1.0"
+__module_description__ = "Some script description"
+ 
+import hexchat # HexChat IRC interface
+import time
+cnc = hexchat.find_context(channel='#THE2021FER')
+emotion = 'none'
+emotion_id = -1
+last_id = -1
+string = ''
+while True:
+    try:    
+        with open("/home/klis/Documents/team_pr/emotion.data", "r") as infile:
+            string = infile.read()
+    except IOError:
+        pass
+    if len(string) > 0:
+        emotion, emotion_id = string.split()
+        emotion_id = int(emotion_id)
+    if emotion is not '':
+        print(emotion)
+        print(emotion_id)
+        if last_id is not emotion_id:
+            hexchat.command(f'say {emotion} {emotion_id}')
+        #time.sleep(1)
+        last_id = emotion_id
+
